edea043a1fe769fb6939e819adc9df341bd8f605
Jackie Huynh <jackiehuynh@Jackies-MacBook-Pro.local>
2016-04-19 15:29:48 -0700

    Added try/except

--- number-game.py
+++ number-game.py
@@ -1,9 +1,11 @@
+"""Simple number guessing game."""
+
 from random import choice
 
 
 random_number = int(choice(range(1, 101)))
 
-raw_guess = raw_input("Please guess a number between 1-100.")
+raw_guess = raw_input("Please guess a number between 1-100: ")
 
 guesses = []
 
@@ -14,22 +16,22 @@ try:
 
         if guess > 100:
             print "This guess is out of range."
-            guess = raw_input("Please guess a number between 1-100.")
+            guess = raw_input("Please guess a number between 1-100: ")
         elif guess < 0:
             print "This guess is out of range."
-            guess = raw_input("Please guess a number between 1-100.")
+            guess = raw_input("Please guess a number between 1-100: ")
         else:
             if guess in guesses:
                 print "You have already guessed this number."
-                guess = raw_input("Please guess another number between 1-100.")
+                guess = raw_input("Please guess another number between 1-100: ")
             elif guess > random_number:
                 print "Your guess is too high."
                 guesses.append(guess)
-                guess = raw_input("Please guess another number between 1-100.")
+                guess = raw_input("Please guess another number between 1-100: ")
             elif guess < random_number:
                 print "Your guess is too low."
                 guesses.append(guess)
-                guess = raw_input("Please guess another number between 1-100.")
+                guess = raw_input("Please guess another number between 1-100: ")
 
     print "You win! It took {} guesses".format(len(guesses))
 
