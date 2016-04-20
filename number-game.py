7d12aee4bae7ccf48f3a6065a246448e46cda38f
Jackie Huynh <jackiehuynh@Jackies-MacBook-Pro.local>
2016-04-20 13:16:01 -0700

    Started rewriting in recursion

--- number-game.py
+++ number-game.py
@@ -1,40 +1,55 @@
-"""Simple number guessing game."""
+"""Simple number guessing game with recursion."""
 
-from random import choice
+from random import randint
 
 
-random_number = int(choice(range(1, 101)))
+def number_game(raw_guess, random_number):
+
+    guesses = []
+
+    while True:
+        try:
+            guess = int(raw_guess)
+            break
+
+        except ValueError:
+            print "This is not a number."
+
+    # base case
+    if raw_guess == random_number:
+        print "You win! It took {} guesses".format(len(guesses))
+
+    else:
+
+        if guess > 100:
+            raw_guess = raw_input("This guess is out of range. Please guess a number between 1-100: * ")
+            print random_number
+            number_game(raw_guess, random_number)
+        elif guess < 0:
+            raw_guess = raw_input("This guess is out of range. Please guess a number between 1-100: ** ")
+            print random_number
+            number_game(raw_guess, random_number)
+        else:
+            if guess in guesses:
+                # print guesses
+                raw_guess = raw_input("You have already guessed this number. Please guess another number between 1-100: ")
+                print random_number
+                number_game(raw_guess, random_number)
+            elif guess > random_number:
+                guesses.append(guess)
+                raw_guess = raw_input("Your guess is too high. Please guess another number between 1-100: ")
+                print random_number
+                number_game(raw_guess, random_number)
+            elif guess < random_number:
+                guesses.append(guess)
+                raw_guess = raw_input("Your guess is too low. Please guess another number between 1-100: ")
+                print random_number
+                number_game(raw_guess, random_number)
+
+    # print "You win! It took {} guesses".format(len(guesses))
 
 raw_guess = raw_input("Please guess a number between 1-100: ")
 
-guesses = []
+random_number = int(randint(1, 100))
 
-try:
-    guess = int(raw_guess)
-
-    while guess != random_number:
-
-        if guess > 100:
-            print "This guess is out of range."
-            guess = raw_input("Please guess a number between 1-100: ")
-        elif guess < 0:
-            print "This guess is out of range."
-            guess = raw_input("Please guess a number between 1-100: ")
-        else:
-            if guess in guesses:
-                print "You have already guessed this number."
-                guess = raw_input("Please guess another number between 1-100: ")
-            elif guess > random_number:
-                print "Your guess is too high."
-                guesses.append(guess)
-                guess = raw_input("Please guess another number between 1-100: ")
-            elif guess < random_number:
-                print "Your guess is too low."
-                guesses.append(guess)
-                guess = raw_input("Please guess another number between 1-100: ")
-
-    print "You win! It took {} guesses".format(len(guesses))
-
-except ValueError:
-    print "This is not a number."
-    raw_guess = raw_input("Please guess a number between 1-100.")
+number_game(raw_guess, random_number)
